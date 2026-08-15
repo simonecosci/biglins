de900821c19bfd58edc9c20a22b1ac970b73bda3
Simone Cosci <simone.cosci@gmail.com>
2026-08-15 10:53:58 +0200

    feat: template nginx listen ports for rootless support

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
+[ -f /run/app-ports.env ] && . /run/app-ports.env
+
+HTTP_PORT="${HTTP_PORT:-80}"
+HTTPS_PORT="${HTTPS_PORT:-443}"
 SSL_MODE="${SSL_MODE:-none}"
 
 if [ "$SSL_MODE" = "none" ]; then
-    curl -fsS http://localhost/ -o /dev/null
+    curl -fsS "http://localhost:${HTTP_PORT}/" -o /dev/null
 else
-    curl -fsSk https://localhost/ -o /dev/null
+    curl -fsSk "https://localhost:${HTTPS_PORT}/" -o /dev/null
 fi
